84fa34aa9816483189bd228272713a4181fc4286
vermouthA28 <147418988+vermouthA28@users.noreply.github.com>
2023-11-26 17:51:24 +0000

    [Finish]

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,5 +4,5 @@ class Book < ApplicationRecord
 
 
   validates :title, presence: true
-  validates :body, presence: true, length: { in: 1..200 }
+  validates :body, presence: true, length: { maximum: 200 }
 end
